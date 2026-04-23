5667fe7d41da4e1d90bb6e7a469142a8422045aa
amuhajirs <amuhajir.syamlan@gmail.com>
2026-04-23 18:54:52 +0700

    fix: change db url default

--- src/database/session.py
+++ src/database/session.py
@@ -7,7 +7,7 @@ from src.database.entity.camera import Camera
 from src.database.entity.token import Token
 
 # URL default untuk SQLite (async)
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite+aiosqlite:///./database.db")
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite+aiosqlite:///./storage/private/database.db")
 
 # Private variables untuk caching
 _engine: AsyncEngine = None
